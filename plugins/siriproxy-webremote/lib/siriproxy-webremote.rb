0959e94c5fd352b08301b7335dfda32f82139d57
Carl Leitner <litlfred@ibiblio.org>
2011-12-03 11:48:43 -0500

    fixed some regexp issues

--- plugins/siriproxy-webremote/lib/siriproxy-webremote.rb
+++ plugins/siriproxy-webremote/lib/siriproxy-webremote.rb
@@ -13,17 +13,16 @@ class SiriProxy::Plugin::WebRemote < SiriProxy::Plugin
 
   
   #demonstrate capturing data from the user (e.x. "Siri proxy number 15")
-  listen_for /vanilla (\s+) (\s+)/i do |m|
-    say "web remote honey: {#m.inspect}"
-    
+  listen_for /vanilla (\w+)/i do |m|
+    say "web remote vanilla $1"    
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
 
 
   #demonstrate capturing data from the user (e.x. "Siri proxy number 15")
-  listen_for /honey (\s+) ((\s+) )+/i do |m|
-    say "web remote honey: {#m.inspect}"
-    
+  listen_for /honey (\w+) ((\w+) )+/i do |m,n|
+    say "web remote honey: #{m}"    
+    say "web remote honey: #{n}"    
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
 
