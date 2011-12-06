1261b85aa76923a9bd6fc69a41d92b2680e1087a
Carl Leitner <litlfred@ibiblio.org>
2011-12-06 09:49:08 -0500

    added a hello world and numbers plugins (stolen graciously from the example plugin) mainly for testing purposes

---
+++ plugins/siriproxy-ping/lib/siriproxy-numbers.rb
@@ -0,0 +1,21 @@
+require 'cora'
+require 'siri_objects'
+
+#######
+# This is a "hello world" style plugin. It simply intercepts the phrase "test siri proxy" and responds
+# with a message about the proxy being up and running (along with a couple other core features). This 
+# is good base code for other plugins.
+# 
+# Remember to add other plugins to the "config.yml" file if you create them!
+######
+
+class SiriProxy::Plugin::Numbers < SiriProxy::Plugin
+  
+  #demonstrate capturing data from the user (e.x. "Siri proxy number 15")
+  listen_for /my number is ([0-9,]*[0-9])/i do |number|
+    say "Detected number: #{number}"
+    
+    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+  end
+
+end
